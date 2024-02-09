98439833684a1d3e6bc45d1fec2d80959efdfa6b
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-02-09 10:08:11 -0300

    [4B][87] Disabling Buttons Conditionally (#38)

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -30,7 +30,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
         {gameBoard.map((row, ri) => <li key={ri}>
             <ol>
                 {row.map((playerSymbol, ci) => <li key={ci}>
-                    <button onClick={() => onSelectSquare(ri, ci)}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(ri, ci)} disabled={playerSymbol !== null}>
+                        {playerSymbol}
+                    </button>
                 </li>)}
             </ol>
         </li>)}
