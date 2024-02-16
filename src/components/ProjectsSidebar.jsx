11ea6d27d96c7d50271256ada45e89578ebff8f1
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-02-16 10:30:10 -0300

    [9][154] Handling Project Creation & Updating the UI (#70)
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39836528#overview

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function ProjectsSidebar({ onStartAddProject }) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
     return <aside className={[
         'w-1/3',
         'px-8',
@@ -22,6 +22,13 @@ export default function ProjectsSidebar({ onStartAddProject }) {
         <div>
             <Button onClick={onStartAddProject}>+ Add Project</Button>
         </div>
-        <ul></ul>
+
+        <ul className="mt-8">
+            {projects.map(project => <li key={project.id}>
+                <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-400 hover:bg-stone-800">
+                    {project.title}
+                </button>
+            </li>)}
+        </ul>
     </aside>;
 }
