14f16853f32037268afe31bd371025ab063585e0
FelipeRamosDev <felipe@feliperamos.info>
2024-02-16 06:29:32 -0300

    [8][139] Forwarding Refs to Custom Components
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39836360#overview

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,8 @@
-export default function ResultModal({ result, targetTime }) {
-    return (<>
-        <dialog className="result-modal" open>
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+    return (
+        <dialog ref={ref} className="result-modal">
             <h2>You {result}</h2>
             <p>The targer time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -9,5 +11,7 @@ export default function ResultModal({ result, targetTime }) {
                 <button>Close</button>
             </form>
         </dialog>
-    </>);
-}
+    );
+});
+
+export default ResultModal;
