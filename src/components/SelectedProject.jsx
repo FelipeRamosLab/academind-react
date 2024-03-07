b8954a5084d263489fe9de0f5a0bb2f7d7d42d46
FelipeRamosDev <felipe@feliperamos.info>
2024-03-07 15:51:44 -0300

    [9][160] Managing Tasks & Understanding Prop Drilling
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39836556#overview

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import Tasks from "./Tasks";
 
-export default function SelectedProject({ project, onDelete }) {
+export default function SelectedProject({ project, tasks, onDelete, onAddTask, onDeleteTask }) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
@@ -18,6 +18,6 @@ export default function SelectedProject({ project, onDelete }) {
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
         </header>
 
-        <Tasks />
+        <Tasks tasks={tasks} onAdd={onAddTask} onDelete={onDeleteTask} />
     </div>
 }
