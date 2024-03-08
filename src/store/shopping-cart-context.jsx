1b5051f33d45f0ca9408a8bef1d7ef542c1d86d3
FelipeRamosDev <felipe@feliperamos.info>
2024-03-07 22:53:16 -0300

    [10] Fixing issues found

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -13,7 +13,7 @@ function shoppingCartReducer(state, action) {
             const updatedItems = [...state.items];
 
             const existingCartItemIndex = updatedItems.findIndex(
-                (cartItem) => cartItem.id === id
+                (cartItem) => cartItem.id === action.payload
             );
             const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -24,9 +24,9 @@ function shoppingCartReducer(state, action) {
                 };
                 updatedItems[existingCartItemIndex] = updatedItem;
             } else {
-                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
                 updatedItems.push({
-                    id: id,
+                    id: action.payload,
                     name: product.title,
                     price: product.price,
                     quantity: 1,
@@ -40,14 +40,14 @@ function shoppingCartReducer(state, action) {
         case 'UPDATE_ITEM': {
             const updatedItems = [...state.items];
             const updatedItemIndex = updatedItems.findIndex(
-                (item) => item.id === productId
+                (item) => item.id === action.payload.productId
             );
 
             const updatedItem = {
                 ...updatedItems[updatedItemIndex],
             };
 
-            updatedItem.quantity += amount;
+            updatedItem.quantity += action.payload.amount;
 
             if (updatedItem.quantity <= 0) {
                 updatedItems.splice(updatedItemIndex, 1);
